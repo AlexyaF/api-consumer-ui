4cb57c3038cca980ee5544933dc6533fc84835ef
Alexya Fortunato <allexya.fortunato@hotmail.com>
2024-09-04 18:17:04 -0300

    style options menu

--- public/js/interactions.js
+++ public/js/interactions.js
@@ -3,7 +3,7 @@ function toggleMenu() {
     const image = document.getElementById('logo_icon'); 
     const icons = document.getElementById('menu__icons'); 
     const container_icon = document.querySelector('.menu__icon__container.logo');
-    const container_options = document.querySelector('.menu__icon__container.options')
+    const textOptions  = document.querySelectorAll('.menu__text__options ');
 
     if (menu && image && icons) {
         // Alterna as classes "collapsed" e "expanded" no menu
@@ -17,12 +17,18 @@ function toggleMenu() {
             container_icon.classList.add('hidden');
             icons.classList.add('menu__icons_collapsed'); // Ajuste a classe aqui
             icons.classList.remove('menu__icons_expanded'); // Remove a classe expandida
+            textOptions.forEach(option => {
+                option.classList.add('hidden');
+            });
         } else {
             // Exibe a imagem e aplica a estilização para a versão expandida dos ícones
             image.classList.remove('hidden');
             container_icon.classList.remove('hidden');
             icons.classList.add('menu__icons_expanded'); // Adiciona a classe expandida
             icons.classList.remove('menu__icons_collapsed'); // Remove a classe colapsada
+            textOptions.forEach(option => {
+                option.classList.remove('hidden');
+            });
         }
     } else {
         console.error('Elemento com a classe "menu", ID "logo_icon" ou "menu__icons" não encontrado.');
